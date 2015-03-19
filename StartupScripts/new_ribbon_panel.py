78d99f1acb714ad3f1898bd01cfe7dee17f5be00
daren-thomas <dthomas.ch@gmail.com>
2015-03-19 16:39:23 +0100

    new_ribbon_panel.py is now good enough to publish

--- StartupScripts/new_ribbon_panel.py
+++ StartupScripts/new_ribbon_panel.py
@@ -46,18 +46,18 @@ def add_radio_group(panel):
 
     tb1 = ToggleButtonData("toggleButton1", "Red")
     tb1.ToolTip = "Red Option"
-    tb1.LargeImage = BitmapImage(Uri(os.path.join(EXAMPLES_PATH, 'StartupScripts', 'red.png')))
-    tb1.Image = BitmapImage(Uri(SMALL_IMG_PATH))
+    tb1.LargeImage = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'red.png')))
 
     tb2 = ToggleButtonData("toggleButton2", "Green")
     tb2.ToolTip = "Green Option"
-    tb2.LargeImage = BitmapImage(Uri(os.path.join(EXAMPLES_PATH, 'StartupScripts', 'green.png')))
-    tb2.Image = BitmapImage(Uri(SMALL_IMG_PATH))
+    tb2.LargeImage = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'green.png')))
 
     tb3 = ToggleButtonData("toggleButton3", "Blue")
     tb3.ToolTip = "Blue Option"
-    tb3.LargeImage = BitmapImage(Uri(os.path.join(EXAMPLES_PATH, 'StartupScripts', 'blue.png')))
-    tb3.Image = BitmapImage(Uri(SMALL_IMG_PATH))
+    tb3.LargeImage = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'blue.png')))
 
     radio_button_group.AddItem(tb1)
     radio_button_group.AddItem(tb2)
@@ -74,25 +74,24 @@ def add_push_button(panel):
     push_button.SetContextualHelp(context_help)
 
     push_button.LargeImage = BitmapImage(Uri(LARGE_IMG_PATH))
-    push_button.Image = BitmapImage(Uri(SMALL_IMG_PATH))
 
 
 def add_split_button(panel):
     """add a split button"""
     button_one = PushButtonData("pbButtonOne", "Option one",
                                 DLL_PATH, "HelloWorld")
-    button_one.LargeImage = BitmapImage(Uri(LARGE_IMG_PATH))
-    button_one.Image = BitmapImage(Uri(SMALL_IMG_PATH))
+    button_one.LargeImage = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'one.png')))
 
     button_two = PushButtonData("pbButtonTwo", "Option two",
                                 DLL_PATH, "HelloWorld")
-    button_two.LargeImage = BitmapImage(Uri(LARGE_IMG_PATH))
-    button_two.Image = BitmapImage(Uri(SMALL_IMG_PATH))
+    button_two.LargeImage = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'two.png')))
 
     button_three = PushButtonData("pbButtonThree", "Option three",
                                 DLL_PATH, "HelloWorld")
-    button_three.LargeImage = BitmapImage(Uri(LARGE_IMG_PATH))
-    button_three.Image = BitmapImage(Uri(SMALL_IMG_PATH))
+    button_three.LargeImage = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'three.png')))
 
     split_button = panel.AddItem(SplitButtonData("splitButton", "Split"))
     split_button.AddPushButton(button_one)
@@ -107,7 +106,8 @@ def add_stacked_buttons(panel):
     text_data.Image = BitmapImage(Uri(SMALL_IMG_PATH))
     text_data.Name = "Text Box"
     text_data.ToolTip = "Enter some text here"
-    text_data.LongDescription = """This is text that will appear next to the image when the user hovers the mouse over the control"""
+    text_data.LongDescription = """This is text that will appear next to the image
+        when the user hovers the mouse over the control"""
     text_data.ToolTipImage = BitmapImage(Uri(LARGE_IMG_PATH))
 
     stacked_items = panel.AddStackedItems(text_data, combo_box_data)
@@ -118,6 +118,46 @@ def add_stacked_buttons(panel):
     text_box.ToolTip = "Enter some text"
     text_box.EnterPressed += lambda sender, args: TaskDialog.Show('new_ribbon_panel', sender.Value)
 
+    combo_box = stacked_items[1]
+    combo_box.ItemText = "ComboBox"
+    combo_box.ToolTip = "Select an Option"
+    combo_box.LongDescription = "Select a number or letter"
+
+    member_data_a = ComboBoxMemberData('A', 'Option A')
+    member_data_a.Image = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'a.png')))
+    member_data_a.GroupName = 'Letters'
+    combo_box.AddItem(member_data_a)
+
+    member_data_b = ComboBoxMemberData('B', 'Option B')
+    member_data_b.Image = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'b.png')))
+    member_data_b.GroupName = 'Letters'
+    combo_box.AddItem(member_data_b)
+
+    member_data_c = ComboBoxMemberData('C', 'Option C')
+    member_data_c.Image = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'c.png')))
+    member_data_c.GroupName = 'Letters'
+    combo_box.AddItem(member_data_c)
+
+    member_data_1 = ComboBoxMemberData('1', 'Option 1')
+    member_data_1.Image = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'one_small.png')))
+    member_data_1.GroupName = 'Numbers'
+    combo_box.AddItem(member_data_1)
+
+    member_data_2 = ComboBoxMemberData('2', 'Option 2')
+    member_data_2.Image = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'two_small.png')))
+    member_data_2.GroupName = 'Numbers'
+    combo_box.AddItem(member_data_2)
+
+    member_data_3 = ComboBoxMemberData('3', 'Option 3')
+    member_data_3.Image = BitmapImage(Uri(os.path.join(
+        EXAMPLES_PATH, 'StartupScripts', 'three_small.png')))
+    member_data_3.GroupName = 'Numbers'
+    combo_box.AddItem(member_data_3)
 
 def add_slide_out(panel):
     pass
